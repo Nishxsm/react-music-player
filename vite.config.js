e2eab8e7babd70de0cebe579a22e754eb6945325
Nishxsm <nishantdandnaik156@gmail.com>
2025-03-19 23:50:22 +0530

    github hosting fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-music-player/", // This must match your GitHub repo name
 })
